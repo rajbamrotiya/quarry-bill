7b05b748f8c020a1a4f8f0f6febc501367a752bf
Rajesh Bamroteeya <rajesh@oshara.ca>
2026-05-26 12:15:22 +0530

    - Init project

---
+++ app/Models/Client.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\ClientFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    /** @use HasFactory<ClientFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'mobile_number',
+        'email',
+    ];
+}
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\ClientController;
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'welcome')->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+});
+
+require __DIR__.'/settings.php';
